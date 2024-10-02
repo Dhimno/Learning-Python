b7fe1b34e03a11be7ad9c13822ef5a4988273942
Dhimno <tylerpappas91@gmail.com>
2024-10-02 17:07:47 +0700

    updated recursion

--- recursion.py
+++ recursion.py
@@ -1,15 +1,12 @@
-from math import sqrt
-
-#print(sqrt(16))
-
-
 def add(x,y):
     if y == 0 :
         return x
     else :
         return 1 + add(x,y-1)
     
-#print(add(90,90))
+print(add(1,2))
+print(add(3,4))
+print(add(6,7))
 
 def kurang(x,y):
     if y == 0 :
@@ -31,10 +28,10 @@ def pangkat(x,y):
 #print(pangkat(9,8))
 
 def bagi(x,y):
-    if x == 0:
+    if x < y:
         return 0
     return 1 + bagi(x-y,y)
-#print(bagi(10,2))
+print(bagi(11,2))
 
 
 def akar(x,y):
@@ -46,4 +43,37 @@ def akar(x,y):
 
 print(akar(3,1))
 
+def faktorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x*(faktorial(x-1))
+    
+print(faktorial(6))
+print(faktorial(7))
+
+def faciter(a,b,c):
+    if a == b:
+        return c * b
+    else:
+        return faciter(a,b+1,c*b)
+
+def fac(n):
+    return faciter(n,1,1)
+
+print(fac(6))
+print(fac(7))
+
+def fibonacci(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return fibonacci(x-1)+fibonacci(x-2)
+    
+print(fibonacci(0))
+print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(3))
 
