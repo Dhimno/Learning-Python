0e79ae4304826f21bf714c9a1f011f6766f2459c
Dhimno <tylerpappas91@gmail.com>
2024-10-05 23:52:56 +0700

    Update NilaiMahasiswa.py

--- Latihan UTS/NilaiMahasiswa.py
+++ Latihan UTS/NilaiMahasiswa.py
@@ -22,14 +22,26 @@ def getMatematika(x):
     return x[2]
 
 # Operator
+def max2(a,b):
+    return (a + b + abs(a-b)) // 2
+
+def max3(a,b,c):
+    return max2(max2(a,b),c)
+
+def min2(a,b):
+    return (a + b - abs(a-b)) // 2
+
+def min3(a,b,c):
+    return min2(min2(a,b),c)
+
 def hitungRangeNilai(nilai):
-    return max(nilai[0],nilai[1],nilai[2]) - min(nilai[0],nilai[1],nilai[2])
+    return max3(nilai[0],nilai[1],nilai[2]) - min3(nilai[0],nilai[1],nilai[2])
 
 def nilaitertinggi(x):
-    return max(getDaspro(x), getDasis(x), getMatematika(x))
+    return max3(getDaspro(x), getDasis(x), getMatematika(x))
 
 def nilaiterterendah(x):
-    return min(getDaspro(x), getDasis(x), getMatematika(x))
+    return min3(getDaspro(x), getDasis(x), getMatematika(x))
 
 # Aplikasi
 print(MakeHasil(getNama(MHS('20010', "Dhimas", NMatkul(100,99,98))),nilaitertinggi(NMatkul(100,99,98)),nilaiterterendah(NMatkul(100,99,98)),hitungRangeNilai(NMatkul(100,99,98))))
