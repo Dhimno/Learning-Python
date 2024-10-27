930d34d854dd2ff01085628f5894cf88f08a34c6
Dhimno <tylerpappas91@gmail.com>
2024-10-28 00:37:35 +0700

    Update Garis.py

--- Tugas Tipe Bentukan 19 October/Garis.py
+++ Tugas Tipe Bentukan 19 October/Garis.py
@@ -1,58 +1,48 @@
-# Tipe Bentukan
-# _type_ Point: <x:_real_, y:_real_>
-#       {<x,y> adalah point dengan x adalah absis dan y adalah ordinat}
-# _type_ Garis: <titik1: Point, titik2: Point>
-#       {<titik1,titik2> adalah titik yang membentuk garis dimana titik 1 adalah titik pangkal dan titik 2 adalah titik ujung}
-# _DEFINISI DAN SPESIFIKASI KONSTRUKTOR_
-# makePoint: 2 _real_ --> Point
-#       {makePoint(x,y) adalah fungsi untuk membuat point dengan 2 inputan real (x,y)}
+# Program     : Garis.py
+# Deskripsi   : Membuat tipe bentukan garis dan mengoperasikannya ke operator dan predikat yang ditentukan
+# NIM/Nama    : 24060124120010/Dhimas Reza Nafi Wahyudi
+# Tanggal     : (28/10/2024)
+# ===========================================================================
+# DEFINISI DAN SPESIFIKASI
+# ===========================================================================
+# TYPE GARIS
+# type Point: <x:real, y:real>
+#       {<x,y> adalah titik x (absis) dan titik y (ordinat)}
+# type Garis: <titik1: Point, titik2: Point>
+#       {<titik1,titik2> adalah garis dimana titik1 adalah titik pangkal dan titik2 adalah titik ujung}
+# DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+# makePoint: <x:real, y:real> --> Point
+#       {makePoint(x,y) adalah sebuah konstruktor untuk mengubah x (absis) dan y (ordinat) menjadi sebuah titik (point)}
 # makeGaris: 2 Point --> Garis
-#       {makeGaris(titik1,titik2) adalah fungsi untuk membuat sebuah garis dengan 2 inputan berupa 2 Point}
-# _DEFINISI DAN SPESIFIKASI SELEKTOR_
-# getAbsis: Point --> _real_
-#       {getAbsis(x): mengambil nilai absis dalam sebuah point}
-# getOrdinat: Point --> _real_
-#       {getOrdinat(y): mengambil nilai ordinat dalam sebuah point}
+#       {makeGaris(titik1,titik2) adalah sebuah konstruktor untuk mengubah 2 point (menghubungkan) menjadi sebuah garis}
+# DEFINISI DAN SPESIFIKASI SELEKTOR
+# getAbsis: Point --> real
+#       {getAbsis(x): sebuah selektor untuk mengambil nilai absis dari point}
+# getOrdinat: Point --> real
+#       {getOrdinat(y): sebuah selektor untuk mengambil nilai ordinat dari point}
 # getTitikPangkal: Garis --> Point
-#       {getTitikPangkal(x): mengambil nilai titik pangkal dalam sebuah garis}
+#       {getTitikPangkal(x): sebuah selektor untuk mengambil nilai point (titik ujung) dari garis}
 # getTitikUjung: Garis --> Point
-#       {getTitikUjung(x): mengambil nilai titik ujung dalam sebuah garis}
-# _DEFINISI DAN SPESIFIKASI OPERATOR_
-# panjangGaris: Garis --> _real_
-#       {panjangGaris(x): menghitung panjang Garis dimana 2 nilai Point dimasukkan kedalam rumus yang sudah ditentukan}
-# IsKuadran3?: Garis --> _boolean_
-#       {IsKuadran3?(x): mengecek apakah titik ujung dan titik pangkal dari sebuah garis dalam kuadran 3, output true jika kedua titik berada pada kuadran 3, false jika sebaliknya}
-# _DEFINISI DAN SPESIFIKASI FUNGSI ANTARA_
-# FX2: _real_ --> _real_
-#       {FX2(x): mengkuadratkan input dengan rumus x*x}
-# _REALISASI_
-# FX2(x): 
-#   x * x
-# panjangGaris(x): 
-#   (FX2(getAbsis(getTitikPangkal(x)) - getAbsis(getTitikUjung(x))) + FX2(getOrdinat(getTitikPangkal(x)) - getOrdinat(getTitikUjung(x))))**0.5
-# IsKuadran3?(X):
-#   _if_ getAbsis(getTitikPangkal(x)) < 0 _and_ getOrdinat(getTitikPangkal(x)) < 0 _and_ getAbsis(getTitikUjung(x)) < 0 and getOrdinat(getTitikUjung(x)) < 0 _then_
-#       _true_
-#   _else_
-#       _false_
-# _APLIKASI_
-# panjangGaris(makeGaris(makePoint(0,0), makePoint(0,8))) --> 8.0
-# panjangGaris(makeGaris(makePoint(0,0), makePoint(0,9))) --> 9.0
-# panjangGaris(makeGaris(makePoint(0,0), makePoint(0,10))) --> 10.0
-# IsKuadran3(makeGaris(makePoint(-2,-8), makePoint(-7,-9))) --> _true_
-# IsKuadran3(makeGaris(makePoint(2,-8), makePoint(-7,-9))) --> _false_
-# IsKuadran3(makeGaris(makePoint(-2,-8), makePoint(7,-9))) --> _false_
-
-
-
-# Konstruktor
+#       {getTitikUjung(x): sebuah selektor untuk mengambil nilai point (titik pangkal) dari garis}
+# DEFINISI DAN SPESIFIKASI OPERATOR
+# panjangGaris: Garis --> real
+#       {panjangGaris(G): adalah operator untuk menghitung panjang garis}
+# Gradien: Garis --> real
+#       {Gradien(G): adalah operator untuk menghitung gradien dari sebuah garis}
+# DEFINISI DAN SPESIFIKASI PREDIKAT
+# IsSejajar?: 2 Garis --> real
+#       {IsSejajar?(G1,G2): adalah predikat untuk mengecek apakah garis 1 dan 2 sejajar (memiliki nilai gradien yang sama)}
+# IsTegakLurus?: 2 Garis --> real
+#       {IsSejajar?(G1,G2): adalah predikat untuk mengecek apakah garis 1 dan 2 tegak lurus (gradien garis1 * gradien garis2 = -1)}
+# ===========================================================================
+# REALISASI
+# ===========================================================================
 def makePoint(x,y):
     return [x,y]
 
 def makeGaris(titik1, titik2):
     return [titik1, titik2]
 
-# Selektor
 def getAbsis(x):
     return x[0]
 
@@ -65,16 +55,21 @@ def getTitikPangkal(x):
 def getTitikUjung(x):
     return x[1]
 
-# Operator
-def panjangGaris(x):
-    return ((getAbsis(getTitikPangkal(x)) - getAbsis(getTitikUjung(x)))**2 + (getOrdinat(getTitikPangkal(x)) - getOrdinat(getTitikUjung(x)))**2)**0.5
+def panjangGaris(G):
+    return ((getAbsis(getTitikPangkal(G)) - getAbsis(getTitikUjung(G)))**2 + (getOrdinat(getTitikPangkal(G)) - getOrdinat(getTitikUjung(G)))**2)**0.5
 
-def IsKuadran3(x):
-    if getAbsis(getTitikPangkal(x)) < 0 and getOrdinat(getTitikPangkal(x)) < 0 and getAbsis(getTitikUjung(x)) < 0 and getOrdinat(getTitikUjung(x)) < 0:
-        return True
-    else:
-        return False
+def Gradien(G):
+    return (getOrdinat(getTitikUjung(G)) - getOrdinat(getTitikPangkal(G))) / (getAbsis(getTitikUjung(G)) - getAbsis(getTitikPangkal(G)))
 
+def IsSejajar(G1,G2):
+    return Gradien(G1) == Gradien(G2)
 
+def IsTegakLurus(G1,G2):
+    return Gradien(G1) * Gradien(G2) == -1
+
+# ===========================================================================
+# APLIKASI
 print(panjangGaris(makeGaris(makePoint(0,0), makePoint(0,8))))
-print(IsKuadran3(makeGaris(makePoint(-2,-8), makePoint(-7,-9))))
+print(Gradien(makeGaris(makePoint(0,0), makePoint(4,8))))
+print(IsSejajar(makeGaris(makePoint(0,0), makePoint(4,8)), makeGaris(makePoint(2,4), makePoint(4,8))))
+print(IsTegakLurus(makeGaris(makePoint(0,0), makePoint(1,1)), makeGaris(makePoint(1,-1), makePoint(0,0))))
