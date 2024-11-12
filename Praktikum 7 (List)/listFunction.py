65307a6e6131ebf6e7b38e5ed2f411e48eb1ee8a
Dhimno <tylerpappas91@gmail.com>
2024-11-12 11:22:23 +0700

    Update listFunction.py

--- Praktikum 7 (List)/listFunction.py
+++ Praktikum 7 (List)/listFunction.py
@@ -2,9 +2,11 @@
 # Deskripsi : Fungsi-fungsi untuk operasi list
 # NIM/Nama  : 24060124120010/Dhimas Reza Nafi Wahyudi
 # Tanggal   : 30/10/2024
-
-#REALISASI SELEKTOR AGAR FUNGSI BISA BERJALAN
+# ============================================================================================================================================
+# DEFINISI DAN SPESIFIKASI SERTA REALISASI
+# ============================================================================================================================================
 # Head: List tidak kosong -> List
+# Head(L) mengoutput elemen dari list kecuali elemen terakhirnya
 def Head(L):
     if IsEmpty(L):
         return None
@@ -12,6 +14,7 @@ def Head(L):
         return L[:-1]
 
 # Tail: List tidak kosong -> boolean
+# Tail(L) mengoutput elemen dari list kecuali elemen pertamanya
 def Tail(L):
     if IsEmpty(L):
         return None
@@ -20,10 +23,12 @@ def Tail(L):
 
 # REALISASI KONSTRUKTOR
 # Konso: elemen, List -> List
+# Konso(e,L) menyatukan sebuah elemen dengan sebuah list dan menaruhnya di depan
 def Konso(e, L):
     return [e] + L
 
 # Konsi: List, elemen -> List
+# Konsi(e,L) menyatukan sebuah elemen dengan sebuah list dan menaruhnya di belakang
 def Konsi(L, e):
     return L + [e]
 
@@ -32,10 +37,12 @@ def Konsi(L, e):
 
 # REALISASI PREDIKAT
 # IsEmpty: List -> boolean
+# IsEmpty(L) predikat yang mengoutput True apabila sebuah list adalah himpunan kosong dan False jika sebaliknya
 def IsEmpty(L):
     return L == []
 
 # IsOneElmt: List -> boolean
+# IsOneElmt(L) mengoutput True jika sebuah list hanya memiliki satu element dan False jika sebaliknya
 def IsOneElmt(L):
     if IsEmpty(L):
         return False
@@ -43,13 +50,11 @@ def IsOneElmt(L):
         return Tail(L) == [] and Head(L) == []
 
 # print("\nHasil IsEmpty:", IsEmpty([]))
-# print("Hasil IsEmpty:", IsEmpty([1,2,3,4,5]))
-# print("Hasil IsOneElmt:", IsOneElmt([]))
-# print("Hasil IsOneElmt:", IsOneElmt([1]))
 # print("Hasil IsOneElmt:", IsOneElmt([1, 2, 3 ,4 ,5]))
 
 # REALISASI SELEKTOR
 # FirstElmt: List tidak kosong -> elemen
+# FirstElmt(L) selektor untuk mengambil nilai elemen pertama dari list
 def FirstElmt(L):
     if IsEmpty(L):
         return None
@@ -57,38 +62,22 @@ def FirstElmt(L):
         return L[0]
 
 # LastElmt: List tidak kosong -> elemen
+# LastElmt(L) selektor untuk mengambil nilai elemen terakhir dari list
 def LastElmt(L):
     if IsEmpty(L):
         return None
     else: 
         return L[-1]
 
-# Tail: List tidak kosong -> boolean
-def Tail(L):
-    if IsEmpty(L):
-        return None
-    else: 
-        return L[1:]
-
-# Head: List tidak kosong -> List
-def Head(L):
-    if IsEmpty(L):
-        return None
-    else: 
-        return L[:-1]
-
 # print("\nHasil FirstElmt:", FirstElmt([]))
-# print("Hasil FirstElmt:", FirstElmt([1,2,3,4,5]))
-# print("Hasil LastElmt:", LastElmt([]))
 # print("Hasil LastElmt:", LastElmt([1,2,3,4,5]))
 
 # print("\nHasil Tail:", Tail([0]))
-# print("Hasil Tail:", Tail([1,2,3,4,5]))
-# print("Hasil Head:", Head([0]))
 # print("Hasil Head:", Head([1,2,3,4,5]))
 
 # REALISASI OPERATOR
 # NbElmt: List -> integer
+# NbElmt(L) menghitung jumlah elemen pada sebuah list
 def NbElmt(L):
     if IsEmpty(L):
         return 0
@@ -96,8 +85,6 @@ def NbElmt(L):
         return 1 + NbElmt(Tail(L))
 
 # print("\nHasil NbElmt:", NbElmt([]))
-# print("Hasil NbElmt:", NbElmt([0]))
-# print("Hasil NbElmt:", NbElmt([1, 1, 1, 1, 1]))
 # print("Hasil NbElmt:", NbElmt([1, 2, 3, 4, 5]))
 
 # TUGAS REALISASI OPERATOR
@@ -110,7 +97,7 @@ def ElmtkeN(N,L):
         else:
             return ElmtkeN(N-1, Tail(L))
 
-print(ElmtkeN(2, [3,4,5]))
+# print(ElmtkeN(2, [3,4,5]))
 
 # IsMember: elemen, List -> boolean
 # IsMember(X,L) adalah benar jika X adalah elemen list L
@@ -123,7 +110,7 @@ def IsMember(x, L):
         else:
             return (IsMember(x, Head(L)))
 
-print(IsMember(2, [3, 4, 6, 2]))
+# print(IsMember(2, [3, 4, 6, 2]))
 
 # Copy: List -> List
 # Copy(L) : Menghasilkan list yang identik dengan list asal
@@ -133,7 +120,7 @@ def Copy(L):
     else:
         return Konso(FirstElmt(L), Copy(Tail(L)))
 
-print(Copy([2, 3, 4, 5]))
+# print(Copy([2, 3, 4, 5]))
 
 # Inverse: List -> List
 # Inverse(L) : Menghasilkan list L yang dibalik, yaitu yang urutan elemennya
@@ -144,7 +131,7 @@ def Inverse(L):
     else:
         return Konsi(Inverse(Tail(L)), FirstElmt(L))
 
-print(Inverse([3, 70, 40, 55]))
+# print(Inverse([3, 70, 40, 55]))
 
 # Konkat: 2 List -> List
 # Konkat(L1,L2) : Menghasilkan konkatenasi 2 buah list, dengan list L2 "sesudah" list L1
@@ -154,7 +141,7 @@ def Konkat(L1, L2):
     else:
         return Konkat(L1 + [FirstElmt(L2)], Tail(L2))
 
-print(Konkat([10, 14, 60, 100], [35, 66, 90]))
+# print(Konkat([10, 14, 60, 100], [35, 66, 90]))
 
 # SumElmt: List of integer -> integer
 # SumElmt(L) menghasilkan jumlahan dari setiap elemen di list L
@@ -164,14 +151,14 @@ def SumElmt(L):
     else:
         return FirstElmt(L) + SumElmt(Tail(L))
 
-print(SumElmt([1, 2, 3, 4, 5]))
+# print(SumElmt([1, 2, 3, 4, 5]))
 
 # AvgElmt: List of integer -> integer
 # AvgElmt(L) menghasilkan nilai rata-rata
 def AvgElmt(L):
     return int(SumElmt(L)/NbElmt(L))
 
-print(AvgElmt([1, 2, 3, 4, 5]))
+# print(AvgElmt([1, 2, 3, 4, 5]))
 
 # MaxElmt: List of integer -> integer
 # MaxElmt(L) mengembalikan elemen maksimun dari list L
@@ -184,7 +171,7 @@ def MaxElmt(L):
     else:
         return max2(FirstElmt(L), MaxElmt(Tail(L)))
 
-print(MaxElmt([1, 2, 10, 2, 3]))
+# print(MaxElmt([1, 2, 10, 2, 3]))
 
 # MaxNB: List of integer -> <integer, integer>
 # MaxNB(L) menghasilkan <max,countMax>
@@ -211,7 +198,7 @@ def NbOcc(x, L):
 def MaxNb(L):
     return [maxlist(L), NbOcc(maxlist(L), L)]
 
-print(MaxNb([100, 100000, 3000000, 30000000, 30000000, 1000000000]))
+# print(MaxNb([100, 100000, 3000000, 30000000, 30000000, 1000000000]))
 
 # AddList: 2 List of integer -> List of integer
 # AddList(L1,L2) menghasilkan list baru yang setiap elemennya
@@ -222,7 +209,7 @@ def AddList(L1, L2):
     else:
         return Konso((FirstElmt(L1) + FirstElmt(L2)), AddList(Tail(L1), Tail(L2)))
 
-print(AddList([1, 6, 10], [3, 9, 6]))
+# print(AddList([1, 6, 10], [3, 9, 6]))
 
 # IsPalindrom: List of character -> boolean
 # IsPalindrom(L) benar jika L merupakan kata palindrom
@@ -236,5 +223,5 @@ def IsPalindrom(L):
     else:
         return FirstElmt(L) == LastElmt(L) and IsPalindrom(Head(Tail(L)))
 
-print(IsPalindrom(['K', 'A', 'T', 'A', 'K']))
+# print(IsPalindrom(['K', 'A', 'T', 'A', 'K']))
 
